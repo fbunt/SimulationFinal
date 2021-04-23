30feeb0c154af0ecfbe687a7ad12d5471ffdbf98
Fred Bunt <fredrick.bunt@gmail.com>
2021-04-23 16:47:32 -0600

    Fix swapped sizes and lowercase name

--- model.py
+++ model.py
@@ -2,14 +2,18 @@ import torch
 from torch import nn
 
 
-class ThreeBodyMLP(torch.nn.module):
-    def __init__(self, n_in=3, n_hidden=10, n_hidden_size=128, n_out=10):
+class ThreeBodyMLP(torch.nn.Module):
+    def __init__(self, n_hidden=10, n_hidden_size=128):
+        # (t, x2x, x2y)
+        n_in = 3
+        # (x1x, x1y, x2x, x2y, v1x, v1y, v2x, v2y, v3x, v3y)
+        n_out = 10
         super().__init__()
         layers = [nn.Linear(n_in, n_hidden), nn.GELU(inplace=True)]
-        for _ in range(1, n_hidden_size):
-            layers.append(nn.Linear(n_hidden, n_hidden))
+        for _ in range(1, n_hidden):
+            layers.append(nn.Linear(n_hidden_size, n_hidden_size))
             layers.append(nn.GELU(inplace=True))
-        layers.append(nn.Linear(n_hidden, n_out))
+        layers.append(nn.Linear(n_hidden_size, n_out))
         self.model = nn.Sequential(*layers)
 
     def forward(self, x):
