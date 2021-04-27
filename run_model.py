010a027c1eb3abc017be5084c732611a1a68d8a2
Fred Bunt <fredrick.bunt@gmail.com>
2021-04-27 16:28:33 -0600

    Add ability to pull model from snapshot and to specify model output size

--- run_model.py
+++ run_model.py
@@ -10,9 +10,16 @@ from model import ThreeBodyMLP
 from train import SolutionInputDataset, dataset_to_array
 
 
-def load_model(path):
-    model = ThreeBodyMLP().cuda()
-    model.load_state_dict(torch.load(path))
+def load_model(path, n_out):
+    model = ThreeBodyMLP(n_out).cuda()
+    try:
+        model.load_state_dict(torch.load(path))
+    except RuntimeError:
+        # try again but treat as full training snapshot
+        try:
+            model.load_state_dict(torch.load(path)["model"])
+        except Exception as e:
+            raise e
     model.eval()
     return model
 
@@ -35,19 +42,20 @@ def save_solutions(sol_dict):
 def get_model_solution(model, ds):
     y0 = ds.y0
     ds = dataset_to_array(ds)
-    y = np.zeros((len(ds), 12))
     t = ds[:, 0].copy()
     ds = torch.tensor(ds).float().cuda()
     with torch.no_grad():
         out = model(ds).cpu().numpy()
     x1x, x1y, x2x, x2y, *vs = out.T
+    y = np.zeros((len(ds), 12))
     y[:, 0] = x1x
     y[:, 1] = x1y
     y[:, 2] = x2x
     y[:, 3] = x2y
     y[:, 4] = -x1x - x2x
     y[:, 5] = -x1y - x2x
-    y[:, 6:] = np.array(vs).T
+    if len(vs) > 0:
+        y[:, 6:] = np.array(vs).T
     return Solution(y0, t, y)
 
 
@@ -61,6 +69,7 @@ def get_model_out_name(out_dir, f):
 def get_parser():
     p = argparse.ArgumentParser()
     p.add_argument("model_path", type=str, help="Model location")
+    p.add_argument("n_out", type=int, help="Size of model output")
     p.add_argument(
         "data_dir", type=str, help="Directory with solutions to load"
     )
@@ -68,14 +77,14 @@ def get_parser():
     return p
 
 
-def main(model_path, data_dir, out_dir):
+def main(model_path, n_out, data_dir, out_dir):
     if not os.path.isdir(out_dir):
         os.mak(out_dir, exist_ok=True)
-    model = load_model(model_path)
+    model = load_model(model_path, n_out)
     sols = load_solutions(data_dir)
-    sols = {f: (s.y0, SolutionInputDataset(s)) for f, s in sols.items()}
+    sols = {f: SolutionInputDataset(s) for f, s in sols.items()}
     model_sols = {
-        get_model_out_name(out_dir, f): get_model_solution(model, *s)
+        get_model_out_name(out_dir, f): get_model_solution(model, s)
         for f, s in sols.items()
     }
     save_solutions(model_sols)
