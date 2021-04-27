e1989e2f333606a8491323363ea123d7a9ff0ca5
Fred Bunt <fredrick.bunt@gmail.com>
2021-04-26 23:02:32 -0600

    Add ability to choose what type of model to train

--- model.py
+++ model.py
@@ -3,11 +3,13 @@ from torch import nn
 
 
 class ThreeBodyMLP(torch.nn.Module):
-    def __init__(self, n_hidden=10, n_hidden_size=128):
+    def __init__(self, n_out=10, n_hidden=10, n_hidden_size=128):
         # (t, x2x, x2y)
         n_in = 3
-        # (x1x, x1y, x2x, x2y, v1x, v1y, v2x, v2y, v3x, v3y)
-        n_out = 10
+        # n_out:
+        # full 10: (x1x, x1y, x2x, x2y, v1x, v1y, v2x, v2y, v3x, v3y)
+        # pos 4: (x1x, x1y, x2x, x2y)
+        # vel 6: (v1x, v1y, v2x, v2y, v3x, v3y)
         super().__init__()
         layers = [
             nn.Linear(n_in, n_hidden_size),
