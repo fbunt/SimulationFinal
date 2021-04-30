62f9a48c5357614f23e417f2af051c0b3ac693a5
Fred Bunt <fredrick.bunt@gmail.com>
2021-04-30 05:53:14 -0600

    Add ability to plot energy error

--- energy.py
+++ energy.py
@@ -47,7 +47,23 @@ def energy(b1, b2, b3):
     return ke, pe, tot
 
 
-def plot(t, ke, pe, tot, ax, xlabels):
+def energy_single(y):
+    r1 = y[:2]
+    r2 = y[2:4]
+    r3 = y[4:6]
+    v1 = np.linalg.norm(y[6:8])
+    v2 = np.linalg.norm(y[8:10])
+    v3 = np.linalg.norm(y[10:])
+    ke = 0.5 * (v1 ** 2 + v2 ** 2 + v3 ** 2)
+    pe = (
+        (-1 / np.linalg.norm(r1 - r2))
+        - (1 / np.linalg.norm(r2 - r3))
+        - (1 / np.linalg.norm(r1 - r3))
+    )
+    return ke + pe
+
+
+def plot_energies(t, ke, pe, tot, ax, xlabels):
     ax.plot(t, ke, "tab:green", label="KE")
     ax.plot(t, pe, "tab:blue", label="PE")
     ax.plot(t, tot, "tab:orange", label="Total")
@@ -58,6 +74,14 @@ def plot(t, ke, pe, tot, ax, xlabels):
     ax.grid()
 
 
+def plot_energy_error(t, error, ax, xlabels):
+    ax.plot(t, error)
+    if xlabels:
+        ax.set_xlabel("Time")
+    ax.set_ylabel("Energy Error")
+    ax.grid()
+
+
 def load_data(fname):
     with open(fname, "rb") as fd:
         return pickle.load(fd)
@@ -65,24 +89,44 @@ def load_data(fname):
 
 def get_parser():
     p = argparse.ArgumentParser()
+    p.add_argument(
+        "-e", "--error", action="store_true", help="Plot energy errors"
+    )
     p.add_argument("paths", nargs="+", help="Path(s) to solution file(s)")
     return p
 
 
-def main(paths):
+def main(paths, error):
     sols = [load_data(path) for path in paths]
     energies = [energy(*solution_to_bodies(sol)) for sol in sols]
     fig = plt.figure(figsize=(10, 10))
-    if len(paths) == 1:
-        ax = fig.add_subplot(111)
-        ax.set_title(paths[0])
-        plot(sols[0].t, *energies[0], ax, True)
+    if not error:
+        if len(paths) == 1:
+            ax = fig.add_subplot(111)
+            ax.set_title("Validation Solution")
+            plot_energies(sols[0].t, *energies[0], ax, True)
+        else:
+            ax1, ax2 = fig.subplots(2, sharex=True, sharey=True)
+            ax1.set_title("Validation Solution")
+            plot_energies(sols[0].t, *energies[0], ax1, False)
+            ax2.set_title("Model Solution")
+            plot_energies(sols[1].t, *energies[1], ax2, True)
     else:
-        ax1, ax2 = fig.subplots(2, sharex=True, sharey=True)
-        ax1.set_title(paths[0])
-        plot(sols[0].t, *energies[0], ax1, False)
-        ax2.set_title(paths[1])
-        plot(sols[1].t, *energies[1], ax2, True)
+        initial_energies = [energy_single(s.y0) for s in sols]
+        errs = [
+            np.abs(ie - tot)
+            for ie, (_, _, tot) in zip(initial_energies, energies)
+        ]
+        if len(paths) == 1:
+            ax = fig.add_subplot(111)
+            ax.set_title("Validation Solution")
+            plot_energy_error(sols[0].t, errs[0], ax, True)
+        else:
+            ax1, ax2 = fig.subplots(2, sharex=True, sharey=False)
+            ax1.set_title("Validation Solution")
+            plot_energy_error(sols[0].t, errs[0], ax1, False)
+            ax2.set_title("Model Solution")
+            plot_energy_error(sols[1].t, errs[1], ax2, True)
     plt.show()
 
 
